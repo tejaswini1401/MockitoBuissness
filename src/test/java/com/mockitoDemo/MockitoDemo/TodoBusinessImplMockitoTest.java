c31c8c5e113eb03e6bbd10c7650e9e6403b602a7
tejaswini_14 <shelaketejaswini14@gmail.com>
2024-09-25 15:50:46 +0530

    Added: Capture Argument Mockito

--- src/test/java/com/mockitoDemo/MockitoDemo/TodoBusinessImplMockitoTest.java
+++ src/test/java/com/mockitoDemo/MockitoDemo/TodoBusinessImplMockitoTest.java
@@ -11,6 +11,7 @@ import java.util.Arrays;
 import java.util.List;
 
 import org.junit.Test;
+import org.mockito.ArgumentCaptor;
 import org.mockito.Mockito;
 
 public class TodoBusinessImplMockitoTest {
@@ -47,7 +48,19 @@ public class TodoBusinessImplMockitoTest {
 		verify(todoService).deleteTodo("Learn to Dance");
 		verify(todoService, Mockito.never()).deleteTodo("Learn Spring MVC");
 		verify(todoService, Mockito.never()).deleteTodo("Learn Spring");
-		verify(todoService, Mockito.times(1)).deleteTodo("Learn to Dance");
-		
+		verify(todoService, Mockito.times(1)).deleteTodo("Learn to Dance");		
+	}
+	
+	@Test
+	public void captureArgument() {
+		ArgumentCaptor<String> argumentCaptor = ArgumentCaptor.forClass(String.class);
+		TodoService todoService = mock(TodoService.class);
+		List<String> allTodos = Arrays.asList("Learn Spring MVC",
+				"Learn Spring", "Learn to Dance");
+		when(todoService.retrieveToDos("Ranga")).thenReturn(allTodos);
+	    TodoBusinessImpl todoBuissnessImple = new TodoBusinessImpl (todoService);
+	    todoBuissnessImple.deleteTodoNotRelatedToSpring("Ranga");
+	    verify(todoService).deleteTodo(argumentCaptor.capture());
+	    assertEquals("Learn to Dance", argumentCaptor.getValue());
 	}
 }
