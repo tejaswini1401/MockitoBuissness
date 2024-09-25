1ed82611ddaea29d0235b001ccc7f645c00eb3fb
tejaswini_14 <shelaketejaswini14@gmail.com>
2024-09-25 12:47:04 +0530

    Added: add dependencies

---
+++ src/test/java/com/mockitoDemo/MockitoDemo/FirstMockitoTest.java
@@ -0,0 +1,14 @@
+package com.mockitoDemo.MockitoDemo;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+public class FirstMockitoTest {
+	
+	@Test
+	public void test() {
+		assertTrue(true);
+	}
+
+}
